747a9bd726dedd41b52d8930157d74ab39633c2c
Suyash Yeotikar <suyashyeotikar@Suyashs-MBP.hsd1.md.comcast.net>
2019-10-10 13:34:55 -0400

    Added motor class to the quadrotor with functions to set and get motor speeds and set motor pin

---
+++ quadrotor/motor.h
@@ -0,0 +1,22 @@
+#ifndef motor_h
+#define motor_h
+
+
+#include "Arduino.h"
+
+/**
+ * @class Motor motor.h
+ * @brief Class defining a motor and functions to set and get motor speeds
+ *
+ */
+
+class Motor {
+ public:
+    unsigned int motorSpeed = 0;
+ private:
+    unsigned int motorPin = -1;
+ public:
+    void setMotorSpeed(int speedVoltLevel);
+    unsigned int getMotorSpeed();
+    void setMotorPin(unsigned int pin);
+}
