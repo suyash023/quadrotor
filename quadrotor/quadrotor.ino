583e7bbf566b1288c06d6a103c544aebeba1c09f
Suyash Yeotikar <suyashyeotikar@Suyashs-MBP.hsd1.md.comcast.net>
2019-10-25 14:40:29 -0400

    Added pid controller for roll pitch and yaw control of the object

--- quadrotor/quadrotor.ino
+++ quadrotor/quadrotor.ino
@@ -5,6 +5,7 @@
 
 IMU mpu6050;
 ComplimentaryFilter cf;
+PIDController pitchControl, rollControl, yawControl;
 void setup() {
   // put your setup code here, to run once:
   mpu6050.Setup();
@@ -13,6 +14,11 @@ void setup() {
   Serial.begin(38400);
   cf.SetRollWeight(0.9);
   cf.SetPitchWeight(0.9);
+  rollControl.SetPIDValues(1, 0, 0);
+  pitchControl.SetPIDValues(1, 0, 0);
+  yawControl.SetPIDValues(1, 0, 0);
+
+
 }
 
 void loop() {
@@ -26,7 +32,15 @@ void loop() {
   mpu6050.AccCorrection();
   float rollReading = cf.FilterRollImplementation(  mpu6050.roll_acc, mpu6050.roll_gyro);
   float pitchReading = cf.FilterPitchImplementation( mpu6050.pitch_acc, mpu6050.pitch_gyro);
+  float rollCorrection = rollControl.RunPID( rollReading, 0);
+  float pitchCorrection = pitchControl.RunPID( pitchReading, 0);
+  float yawCorrection = yawControl.RunPID( mpu6050.gyro_x, 0);
   cf.DisplayValues(rollReading, pitchReading, 0);
-   
-  //mpu6050.DisplayValues();
+  Serial.print("Roll correction is: ");
+  Serial.println(rollCorrection);
+  Serial.print("Pitch correction is: ");
+  Serial.println(pitchCorrection);
+  Serial.print("Yaw correction is: ");
+  Serial.println(yawCorrection);
+  delay(10); 
 }
